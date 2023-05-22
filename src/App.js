d594f2220cf531810882e38d49df7ae9a55370d7
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-22 11:54:40 +0200

    Finished stop and reset functions

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import { useState, useEffect } from "react";
 import Time from "./Components/Time/Time";
 
 const App = () => {
-  const [time, setTime] = useState(0);
+  let [time, setTime] = useState(0);
   const [timer, setTimer] = useState(null);
+
 
 const updateTime = () => {
   setTime(prevValue => prevValue + 1);
 }
-
-console.log(timer);
   const start = () => {
-    setTimer(setInterval( updateTime, 1))
+    setTimer(setInterval(updateTime, 1))
   }; 
 
-
-
   // setInterval (function that updates time, time in milseconds)
-  // function that updates time :
-  //  () => {
-  //setTime(prevValue => prevValue + 1) 
-  // }
+
+  const stop = () => {
+    clearInterval(timer);
+  }; 
+
+  const reset = () => {
+    setTime(0);
+    console.log(setTime);
+  }
   
   useEffect(() => {
     return () => {
         if(timer) clearInterval(timer);
-        console.log('stoop');
     };
   }, []);
 
@@ -34,8 +35,8 @@ console.log(timer);
     <div>
       <p>{Time(time)}</p>
       <button onClick={start}>START</button>
-      <button onClick={clearInterval(start)}>STOP</button>
-      <button >RESET</button>
+      <button onClick={stop}>STOP</button>
+      <button onClick={reset}>RESET</button>
     </div>
   );
 }
