f0be29daa8b680937c6083e785fe95bf50078031
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-25 08:31:00 +0200

    Added a functionality to check if there is existing stopcount to prevent multiple stopwatches from running in the background

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './styles.css';
 
 const App = () => {
   let [time, setTime] = useState(0);
-  const [timer, setTimer] = useState(null);
+  let [timer, setTimer] = useState(null);
+  console.log(timer);
 
 
 const updateTime = () => {
   setTime(prevValue => prevValue + 1);
 }
   const start = () => {
+    if (!timer) {
     setTimer(setInterval(updateTime, 1))
+    }
   }; 
+
   const stop = () => {
     clearInterval(timer);
   }; 
 
   const reset = () => {
     setTime(0);
+    setTimer(0);
   }
   
   useEffect(() => {
